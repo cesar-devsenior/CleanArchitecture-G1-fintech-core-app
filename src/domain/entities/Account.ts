8003446361804336a012459588a53bfba411b53c
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2026-08-22 12:26:38 -0500

    Ajuste a valicaciones de dominio

--- src/domain/entities/Account.ts
+++ src/domain/entities/Account.ts
@@ -5,12 +5,12 @@ import { InvalidPropValueError } from "../exceptions/DomainError";
 export type AccountStatus = "ACTIVE" | "FROZEN";
 
 export interface AccountProps {
-  id: string;
+  id?: string;
   accountNumber: string;
   balance: Decimal;
   userId: string;
   status: AccountStatus;
-  createdAt: Date;
+  createdAt?: Date;
 }
 
 export class Account {
@@ -24,43 +24,57 @@ export class Account {
     if (props.balance.isNegative()) {
       throw new InvalidPropValueError('Una cuenta no puede ser inicializada con saldo negativo.');
     }
+    if (props.createdAt === undefined) {
+      props.createdAt = new Date();
+    }
     return new Account(props);
   }
 
   // getters
-  public get id(): string { return this.props.id; }
+  public get id(): string | undefined { return this.props.id; }
   public get accountNumber(): string { return this.props.accountNumber; }
   public get balance(): Decimal { return this.props.balance; }
   public get userId(): string { return this.props.userId; }
   public get status(): AccountStatus { return this.props.status; }
-  public get createdAt(): Date { return this.props.createdAt; }
+  public get createdAt(): Date | undefined { return this.props.createdAt; }
 
   // Comportamientos de dominio
   public deposit(amount: Decimal): void {
     if (amount.lte(0)) {
       throw new InvalidAmountError("El monto del depósito debe ser estrictamente mayor a cero.");
     }
-    this.props.balance = this.props.balance.plus(amount);
+    if (this.props.status === "FROZEN") {
+      throw new AccountFrozenError(this.id!);
+    }
+
+    this.props.balance = this.props.balance.plus(amount); 
   }
 
   public withdraw(amount: Decimal): void {
     if (amount.lte(0)) {
       throw new InvalidAmountError("El monto del retiro debe ser estrictamente mayor a cero.");
     }
-    if( this.props.status === "FROZEN") {
-      throw new AccountFrozenError(this.id);
+    if (this.props.status === "FROZEN") {
+      throw new AccountFrozenError(this.id!);
     }
     if (this.props.balance.lessThan(amount)) {
-      throw new InsufficientBalanceError(this.id);
+      throw new InsufficientBalanceError(this.id!);
     }
+
     this.props.balance = this.props.balance.minus(amount);
   }
 
   public freeze(): void {
+    if (this.props.status === "FROZEN") {
+      throw new InvalidPropValueError("La cuenta ya se encuentra congelada.");
+    }
     this.props.status = "FROZEN";
   }
 
   public unfreeze(): void {
+    if (this.props.status === "ACTIVE") {
+      throw new InvalidPropValueError("La cuenta ya se encuentra activa.");
+    }
     this.props.status = "ACTIVE";
   }
 }
